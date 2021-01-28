1e29865228ed94ab8e2918b12169737dc9a8ebf8
AndreaG <gaggiandrea@gmail.com>
2021-01-28 18:06:37 +0100

    fix

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php 
-    $p = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quasi, necessitatibus animi consequatur aut vitae, nemo fugiat rerum, magnam error quaerat beatae ad dolorum iure! Aliquid praesentium modi provident officiis!";
+<?php
+    $parola = $_GET["parola"];
+    $p = "Questo è una specie di paragrafo placeholder che contiene una parola scritta nell'URL, questa: <span style='color: red'>{$parola}</span>";
 ?>
 
 <!DOCTYPE html>
@@ -9,27 +10,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>First php</title>
         <style>
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-            }
             body {
                 font-size: 1.5rem;
-            }
-            strong {
-                text-transform: uppercase;
             }
         </style>
     </head>
     <body>
-        <h4>Paragrafo</h4>
-        <p style="margin-bottom: 30px;"><?php echo var_dump($p)?></p>
-
-        <h4 style="color: crimson;">Bad Word</h4>
-        <code style="color: purple">index.php?bad=THEBADWORD</code>
-        <p>Scrivere <?php echo str_replace($_GET["bad"], "***", $_GET["bad"]) ?> non va bene.</p>
-
+        <code style="color: purple">index.php?parola=BADWORD</code>
+        <p><?php echo $p?></p>
+        <p>Lunghezza del paragrafo sopra: <?php echo strlen($p)?></p>
+        <p>
+            <?php echo str_replace($parola, "***", $p) ?>
+        </p>
 
     </body>
 </html>
